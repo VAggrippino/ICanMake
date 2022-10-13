09998c7891bc8eaf2aba3acf86f253f316b91b02
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2022-10-13 14:42:49 +0800

    Various changes

---
+++ google.com/js/main.js
@@ -0,0 +1,36 @@
+function fetchOtherApps() {
+    return fetch(`otherApps.html`)
+        .then(response => response.text())
+        .then((rt) => {
+            const rtDom = new DOMParser().parseFromString(rt, `text/html`);
+            console.log(rtDom);
+            const icons = rtDom.querySelector(`.otherApps .icons`).outerHTML;
+            return icons;
+        });
+}
+
+window.addEventListener(`load`, () => {
+    const otherAppsButton = document.querySelector(`.otherAppsButton`);
+    otherAppsButton.addEventListener(`click`, (e) => {
+        e.preventDefault();
+
+        fetchOtherApps().then((icons) => {
+            const otherApps = document.createElement(`DIV`);
+            otherApps.classList.add(`inline`, `otherApps`);
+            otherApps.innerHTML = icons;
+
+            const buttonMiddle = otherAppsButton.offsetLeft + otherAppsButton.offsetWidth / 2;
+            const buttonBottom = otherAppsButton.offsetTop + otherAppsButton.offsetHeight;
+
+            const root = document.querySelector(`:root`);
+            const rootStyle = getComputedStyle(root);
+            const pageWidth = document.body.offsetWidth;
+            const dialogWidth = rootStyle.getPropertyValue(`--oaDialogWidth`);
+            const oaDialogLeft = `calc(${pageWidth}px - ${dialogWidth} - ${buttonMiddle}px)`;
+            root.setProperty(`--oaDialogLeft`, oaDialogLeft);
+
+
+            document.body.appendChild(otherApps);
+        });
+    });
+})
